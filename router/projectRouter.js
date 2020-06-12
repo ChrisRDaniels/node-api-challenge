3ed6ef31d775010b27b78abb5d2e7da8e868a85d
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-06-12 17:37:49 -0600

    all end points tested and readme done, committing  mvp

--- router/projectRouter.js
+++ router/projectRouter.js
@@ -1,100 +1,100 @@
-const express = "express";
-const router = require("express").Router();
+const express = 'express';
+const router = require('express').Router();
 
-const projectdb = require("../data/helpers/projectModel");
+const projectdb = require('../data/helpers/projectModel');
 
-const { validateProjectId } = require("../middleware");
+const { validateProjectId } = require('../middleware');
 
 // GET A LIST OF ALL PROJECTS
-router.get("/", (req, res) => {
- projectdb
-  .get()
-  .then(projects => {
-   res.status(200).json(projects);
-  })
-  .catch(err => {
-   console.log(err);
-   res.status(500).json({
-    message: "There was an error retrieving projects from the database."
-   });
-  });
+router.get('/', (req, res) => {
+  projectdb
+    .get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: 'There was an error retrieving projects from the database.',
+      });
+    });
 });
 
 // GET A PROJECT BY THE ID
-router.get("/:id", validateProjectId, (req, res) => {
- projectdb
-  .get(req.params.id)
-  .then(project => {
-   res.status(200).json(project);
-  })
-  .catch(err => {
-   console.log(err);
-   res
-    .status(500)
-    .json({ message: "There was a problem retrieving project by ID." });
-  });
+router.get('/:id', validateProjectId, (req, res) => {
+  projectdb
+    .get(req.params.id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: 'There was a problem retrieving project by ID.' });
+    });
 });
 
-//GET A PROJECTS ACTIONS
-router.get("/:id/actions", validateProjectId, (req, res) => {
- projectdb
-  .getProjectActions(req.params.id)
-  .then(actions => {
-   res.status(200).json(actions);
-  })
-  .catch(err => {
-   console.log(err);
-   res
-    .status(500)
-    .json({ message: "There was a problem getting project actions." });
-  });
+//GET A PROJECTS ACTIONS BY ID
+router.get('/:id/actions', validateProjectId, (req, res) => {
+  projectdb
+    .getProjectActions(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: 'There was a problem getting project actions.' });
+    });
 });
 
 //ADD A PROJECT
-router.post("/", (req, res) => {
- projectdb
-  .insert(req.body)
-  .then(project => {
-   res.status(201).json(project);
-  })
-  .catch(err => {
-   console.log(err);
-   res
-    .status(500)
-    .json({ message: "There was an error creating the project." });
-  });
+router.post('/', (req, res) => {
+  projectdb
+    .insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: 'There was an error creating the project.' });
+    });
 });
 
 //UPDATE A PROJECT BY ID
-router.put("/:id", validateProjectId, (req, res) => {
- projectdb
-  .update(req.params.id, req.body)
-  .then(project => {
-   res.status(201).json(project);
-  })
-  .catch(err => {
-   console.log(err);
-   res
-    .status(500)
-    .json({ message: "There was an error updating that project." });
-  });
+router.put('/:id', validateProjectId, (req, res) => {
+  projectdb
+    .update(req.params.id, req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: 'There was an error updating that project.' });
+    });
 });
 
-//DELETE A PROJECT
-router.delete("/:id", validateProjectId, (req, res) => {
- projectdb
-  .remove(req.params.id)
-  .then(message => {
-   res.status(200).json({
-    message: `The item with ID ${req.params.id} was successfully deleted.`
-   });
-  })
-  .catch(err => {
-   console.log(err);
-   res.status(500).json({
-    message: "There was a problem deleting the project from the server."
-   });
-  });
+//DELETE A PROJECT BY ID
+router.delete('/:id', validateProjectId, (req, res) => {
+  projectdb
+    .remove(req.params.id)
+    .then((message) => {
+      res.status(200).json({
+        message: `The item with ID ${req.params.id} was successfully deleted.`,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: 'There was a problem deleting the project from the server.',
+      });
+    });
 });
 
 module.exports = router;
